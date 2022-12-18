7870dbfe16cb06e7033e1b4cce1842afafb49927
dytron <guilhermealfaiagui1@gmail.com>
2022-12-18 19:10:55 -0300

    AABB implementada

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
+var points = []
+var btnCreate
+var aabb = new AABB()
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500)
+  btnCreate = createButton('Criar')
+  btnCreate.mousePressed(() => {
+    aabb.fit(points)
+  })
 }
 
 function draw() {
-  background(220);
-  
+  goCartesian()
+  colore('black')
+  points.forEach(p => circle(p.x, p.y, 5))
+  aabb.draw()
+}
+
+function mousePressed() {
+  if (abs(mouseXC) * 2 > width || abs(mouseYC) * 2 > height) return
+  points.push(new vec2(mouseXC, mouseYC))
 }
