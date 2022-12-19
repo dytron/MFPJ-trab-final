e9ef161050086f77c16af32a30f8472957b1870b
dytron <guilhermealfaiagui1@gmail.com>
2022-12-19 00:23:44 -0300

    Mover volume e testar interseção

--- sketch.js
+++ sketch.js
@@ -1,22 +1,134 @@
-var points = []
-var btnCreate
-var aabb = new AABB()
+// Índice do grupo de pontos em modificação
+var groupIndex = 0
+// Grupos de pontos
+var pointsGroup = [[]]
+// Estado
+const STATE = {
+  ADD_POINTS: 0,            // Adicionar pontos
+  SELECT_VOLUME: 1,         // Selecionar volume
+  MOVING_VOLUME: 2          // Movendo volume 
+}
+let state = STATE.ADD_POINTS
+// Botões
+var btnAdd, btnCreate, btnMove
+var volumeList = []
+var selected = {
+  volume: null, // Instância do volume
+  points: [],   // Pontos do volume quando havia sido selecionado
+  group: [],    // Pontos da nuvem quando o volume havia sido selecionado
+  index: -1     // Índice na lista de Volumes / Grupos
+}
+// Cor normal
+var COLOR_DEFAULT = 'rgba(255, 0, 0, 0.5)'
+// Cor em caso de interseção
+var COLOR_OVERLAP = 'rgba(50, 205, 50, 0.5)'
+
 function setup() {
+  cursor('arrow')
   createCanvas(500, 500)
+  // Adicionar pontos
+  btnAdd = createButton('Adicionar Pontos')
+  btnAdd.mousePressed(() => {
+    state = STATE.ADD_POINTS
+  })
+  // Criar Volume
   btnCreate = createButton('Criar')
   btnCreate.mousePressed(() => {
-    aabb.fit(points)
+    let v = new AABB()
+    v.fit(pointsGroup[groupIndex])
+    volumeList.push(v)
+    pointsGroup[++groupIndex] = []
   })
+  // Mover
+  btnMove = createButton('Mover')
+  btnMove.mousePressed(() => {
+    state = STATE.SELECT_VOLUME
+    selectedVolume = null
+  })
+
 }
 
 function draw() {
   goCartesian()
   colore('black')
-  points.forEach(p => circle(p.x, p.y, 5))
-  aabb.draw()
+
+  let type = ARROW
+  let mousePoint = new vec2(mouseXC, mouseYC)
+
+  if (state === STATE.MOVING_VOLUME) {
+    if (selected.volume != null) {
+      let mouseMove = mousePoint.sub(mouseOrigin)
+      selected.volume.points = []
+      for (let i = 0; i < selected.points.length; i++) {
+        selected.volume.points[i] = selected.points[i].add(mouseMove)
+      }
+      for (let i = 0; i < selected.group.length; i++) {
+        pointsGroup[selected.index][i] = selected.group[i].add(mouseMove)
+      }
+    }
+  }
+
+  // Desenhar os pontos de cada grupo de pontos
+  pointsGroup.forEach(
+    group => group.forEach(
+      p => circle(p.x, p.y, 5)
+    )
+  )
+
+  volumeList.forEach(v => {
+    v.fillColor = COLOR_DEFAULT
+    v.outlineSize = 1
+    // Deixa o contorno mais grosso se o mouse estiver dentro
+    if (v.hasPoint(mousePoint)) {
+      v.outlineSize = 3
+      if (state != STATE.ADD_POINTS)
+        type = MOVE
+    }
+  })
+  cursor(type)
+  volumeList.forEach(v => {
+    volumeList.forEach(v2 => {
+      if (v != v2 && v.overlaps(v2)) {
+        v.fillColor = v2.fillColor = COLOR_OVERLAP
+      }
+    })
+  })
+
+  volumeList.forEach(v => v.draw())
 }
 
 function mousePressed() {
+  let mousePoint = new vec2(mouseXC, mouseYC)
+  // Ignora se o clique for fora do canvas
   if (abs(mouseXC) * 2 > width || abs(mouseYC) * 2 > height) return
-  points.push(new vec2(mouseXC, mouseYC))
+  switch(state) {
+    case STATE.ADD_POINTS:
+      pointsGroup[groupIndex].push(new vec2(mouseXC, mouseYC))
+      break
+    case STATE.SELECT_VOLUME:
+    case STATE.MOVING_VOLUME:
+      for (let i = volumeList.length - 1; i >= 0; i--) {
+        v = volumeList[i]
+        if (selected.volume == null && v.hasPoint(mousePoint)) {
+          selected.volume = v
+          selected.points = v.getPoints()
+          selected.index = i
+          selected.group = []
+          pointsGroup[i].forEach(p => {
+            selected.group.push(p.copy())
+          })
+          state = STATE.MOVING_VOLUME
+          mouseOrigin = mousePoint.copy()
+          return
+        }
+      }
+      break
+  }
+}
+
+function mouseReleased() {
+  if (state == STATE.MOVING_VOLUME) {
+    state == STATE.SELECT_VOLUME
+  }
+  selected.volume = null
 }
